303ae66ead9e5f5e9eaf19ad6164b59c5a61ed62
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-09-08 20:04:45 -0300

    modal para btn-contato

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //window.alert("Página em desenvolvimento. >TOTALMENTE ILUSTRATIVO<");
+
+var modal = document.getElementById("modalContato");
+var btn = document.getElementById("contatoBtn");
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
